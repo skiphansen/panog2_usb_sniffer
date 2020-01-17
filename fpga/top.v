bc09a7c2305f16499e497f8d2984a23c7fa1ba14
Skip Hansen <skip@gfrn.org>
2020-01-17 10:51:08 -0800

    Stripped out opl3 stuff, blinky runs.

--- fpga/top.v
+++ fpga/top.v
@@ -14,21 +14,6 @@ module top
     ,input           uart_txd_i
     ,output          uart_rxd_o
 
-    // SPI-Flash
-    ,output          flash_sck_o
-    ,output          flash_cs_o
-    ,output          flash_si_o
-    ,input           flash_so_i
-
-     // WM8750 Codec
-     ,output codec_mclk
-     ,output codec_bclk
-     ,output codec_dacdata
-     ,output codec_daclrck
-     ,input  codec_adcdata
-     ,output codec_adclrck
-     ,inout  codec_scl
-     ,inout  codec_sda
 );
 
 // Generate 32 Mhz system clock and 25 Mhz audio clock from 125 Mhz input clock
@@ -146,22 +131,7 @@ u_top
     ,.gpio_input_i(gpio_in_w)
     ,.gpio_output_o(gpio_out_w)
     ,.gpio_output_enable_o(gpio_out_en_w)
-
-    ,.channel_a(channel_a)
-    ,.channel_b(channel_b)
-    ,.channel_c(channel_c)
-    ,.channel_d(channel_d)
-    ,.sample_clk(sample_clk)
-    ,.sample_clk_128(codec_bclk_i)
 );
-
-//-----------------------------------------------------------------
-// SPI Flash
-//-----------------------------------------------------------------
-assign flash_sck_o = spi_clk_w;
-assign flash_si_o  = spi_si_w;
-assign flash_cs_o  = spi_cs_w[0];
-assign spi_so_w    = flash_so_i;
 
 //-----------------------------------------------------------------
 // GPIO bits
@@ -220,60 +190,6 @@ else
 
 // 'OR' two UARTs together
 assign uart_rxd_o  = txd_q;
-
-
-// Audio
-wire signed [18:0] left_pre, right_pre;
-
-assign left_pre = channel_a + channel_c;
-assign right_pre = channel_b + channel_d;
-
-wire signed [15:0] left, right;
-
-assign left = left_pre > 32767 ? 32767 : left_pre < -32768 ? -32768 : left_pre[15:0];
-assign right = right_pre > 32767 ? 32767 : right_pre < -32768 ? -32768 : right_pre[15:0];
-
-reset_gen reset_gen_25 (
-    .clk_i(clk25)
-    ,.rst_o(reset25)
-);
-
-audio audio_out (
-    .clk25(clk25),
-    .reset25(reset25),
-    .codec_bclk_i(codec_bclk_i),
-    .codec_dacdat(codec_dacdata),
-    .codec_daclrc(codec_daclrck),
-    .codec_adcdat(codec_adcdata),
-    .codec_adclrc(codec_adclrck),
-    .audio_right_sample(right),
-    .audio_left_sample(left),
-    .audio_sample_clk(sample_clk)
-);
-
-ODDR2 mclk_buf (
-    .S(1'b0),
-    .R(1'b0),
-    .D0(1'b1),
-    .D1(1'b0),
-    .C0(clk25),
-    .C1(!clk25),
-    .CE(1'b1),
-    .Q(codec_mclk)
-);
-
-ODDR2 bclk_buf (
-    .S(1'b0),
-    .R(1'b0),
-    .D0(1'b1),
-    .D1(1'b0),
-    .C0(codec_bclk_i),
-    .C1(!codec_bclk_i),
-    .CE(1'b1),
-    .Q(codec_bclk)
-);
-
-
 
 //-----------------------------------------------------------------
 // Tie-offs
